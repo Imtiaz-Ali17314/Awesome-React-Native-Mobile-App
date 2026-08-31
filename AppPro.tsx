70d0f9fdcda25e7a4089d56a3ff6edcd7d1b3fb2
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-31 12:29:39 +0500

    Enhance AppPro component with dark mode support and improve structure

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { JSX } from 'react';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const AppPro = () => {
+const AppPro = (): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View>
-      <Text>AppPro</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+          Hello world, This is a simple AppPro
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default AppPro;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: 'white',
+  },
+  darkText: {
+    color: 'black',
+  },
+});
